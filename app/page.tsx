35b95a63962d1bf6a3c1ffc18c1da2ecfaa38af9
Pedro Arruda <pedroarruda91@gmail.com>
2023-06-30 16:47:23 -0300

    registry.tsx created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSectionComponent from "./components/HeroSection";
 import ModalVideo from "./components/ModalVideo";
+import Navbar from "./components/navBar";
 
 
 export default function Home() {
   return (
    <>
+    <Navbar />
     <HeroSectionComponent />
     <ModalVideo />
     </>
