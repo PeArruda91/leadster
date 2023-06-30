70d76462de686164f20f609ecd71365c62acf1a8
Pedro Arruda <pedroarruda91@gmail.com>
2023-06-30 01:32:09 -0300

    codemon next/image

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const NavBar = styled.nav`
   background-color: #ffffff;
