36941bc52b1298506d250de836607d86387ac988
Pedro Arruda <pedroarruda91@gmail.com>
2023-06-30 21:34:45 -0300

    navBar logo change

--- app/components/ModalVideo.tsx
+++ app/components/ModalVideo.tsx
@@ -19,6 +19,8 @@ const ModalVideo: React.FC = () => {
   const [selectedVideo, setSelectedVideo] = useState<Video | null>(null);
   const [videoUrl, setVideoUrl] = useState<string | null>(null);
   const [videoHeight, setVideoHeight] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const videosPerPage = 6;
 
   useEffect(() => {
     const calculateVideoHeight = () => {
@@ -48,11 +50,14 @@ const ModalVideo: React.FC = () => {
   };
 
   const renderVideoCards = () => {
-    const groups: Video[][] = [];
     const videos = videoData.videos;
+    const indexOfLastVideo = currentPage * videosPerPage;
+    const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
+    const currentVideos = videos.slice(indexOfFirstVideo, indexOfLastVideo);
 
-    for (let i = 0; i < videos.length; i += 3) {
-      const group = videos.slice(i, i + 3);
+    const groups: Video[][] = [];
+    for (let i = 0; i < currentVideos.length; i += 3) {
+      const group = currentVideos.slice(i, i + 3);
       groups.push(group);
     }
 
@@ -68,10 +73,47 @@ const ModalVideo: React.FC = () => {
     ));
   };
 
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const pageNumbers = Math.ceil(videoData.videos.length / videosPerPage);
+  const pagination = [];
+  for (let i = 1; i <= pageNumbers; i++) {
+    pagination.push(
+      <button
+        key={i}
+        onClick={() => paginate(i)}
+        style={{
+          backgroundColor: "transparent",
+          border: "none",
+          borderRadius: "50%",
+          color: "#0084FF",
+          padding: "8px 12px",
+          margin: "0 5px",
+          cursor: "pointer",
+        }}
+      >
+        {i}
+      </button>
+    );
+  }
+
   return (
     <div>
       {/* Video cards */}
       <div>{renderVideoCards()}</div>
+
+      {/* Pagination */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        {pagination}
+      </div>
 
       {/* Modal */}
       {isOpen && videoUrl && (
@@ -88,7 +130,10 @@ const ModalVideo: React.FC = () => {
               allow="autoplay; encrypted-media"
               allowFullScreen
             ></iframe>
-            <button onClick={() => window.open(videoUrl, "_blank")}>
+            <button
+              onClick={() => window.open(videoUrl, "_blank")}
+              style={{ backgroundColor: "#0084FF", color: "#FFFFFF" }}
+            >
               Watch Video
             </button>
           </ModalContent>
--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 const NavBar = styled.nav`
   background-color: #ffffff;
@@ -16,17 +16,22 @@ const LogoWrapper = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 const Logo = styled(Image)`
-  max-height: 50%;
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+  transform: scale(1.5); /* Reduz a altura e largura em 25% */
 `;
 
 const Navbar = () => {
   return (
     <NavBar>
       <LogoWrapper>
-        <Logo src="/media/logo.png" alt="Logo" layout="fixed" width={120} height={40} />
+        <Logo src="/media/logo.png" alt="Logo" width={120} height={40} />
       </LogoWrapper>
     </NavBar>
   );
